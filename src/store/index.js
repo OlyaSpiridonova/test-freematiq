4e9bac8a2bc4c691da5b3e7af424bd170bdc24d1
Olya <Spiridonova.olya14@gmail.com>
2023-02-23 10:27:51 +0700

    Redux-persist - для сохранения состояния

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,15 @@ import { createStore } from 'redux';
 import { reducers } from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(reducers, composeWithDevTools());
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(persistedReducer, composeWithDevTools());
+export const persistor = persistStore(store);
